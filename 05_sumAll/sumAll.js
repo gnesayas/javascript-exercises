9a3cdb8b1478f431211144cb65ed7fcd2b6bb35f
Gabriel <63373336+gnesayas@users.noreply.github.com>
2023-08-21 18:22:48 -0500

    Add code to pass tests for exercises 1 through 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(m, n) {
+    if (typeof m !== 'number' || typeof n !== 'number' ||
+        m < 0 || n < 0) {
+        return 'ERROR';
+    }
+    const lo = m < n ? m : n;
+    const hi = m > n ? m : n;
+    let sum = 0;
+    for (let i = lo; i <= hi; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
