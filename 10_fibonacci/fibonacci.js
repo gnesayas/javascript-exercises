97170faee2adda465af3416341d2222211d10d21
Gabriel <63373336+gnesayas@users.noreply.github.com>
2023-08-25 19:46:36 -0500

    Complete remaining javascript exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (parseInt(num) < 0) return "OOPS";
+    let current = 1;
+    let next = 1;
+    let i = 1;
+    while (i < num) {
+        let temp = current;
+        current = next;
+        next += temp;
+        i++;
+    }
+    return current;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, current) => {
+        let oldestDeathYear = oldest.yearOfDeath ? oldest.yearOfDeath : (new Date()).getFullYear();
+        let currentDeathYear = current.yearOfDeath ? current.yearOfDeath : (new Date()).getFullYear();
+        if ((oldestDeathYear - oldest.yearOfBirth) > (currentDeathYear - current.yearOfBirth)) {
+            return oldest;
+        }
+        return current;
+    });
 };
 
 // Do not edit below this line
